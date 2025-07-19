41fbf51fd7ec92c8b9a2a989041f5eeaceb0c38d
Caio Gustavo <caiogggustavo49@gmail.com>
2025-07-19 18:16:48 -0300

    add delete btn

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
@@ -15,13 +16,29 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
-                return _context.Seller.ToList();
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj=> obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        internal static object FindById(object value)
+        {
+            throw new NotImplementedException();
         }
     }
 }
